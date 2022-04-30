7bfd7e65ad89b11e3da09db0c32811851345d4ec
crizostomo <diogo_crizostomo@hotmail.com>
2022-04-30 20:24:05 -0300

    37. Integration Test for POST endpoint - /v1/libraryevent - Part 2

---
+++ library-events-producer/src/test/java/intg/com/kafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.kafka.controller;
+
+import com.kafka.domain.Book;
+import com.kafka.domain.LibraryEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LibraryEventsControllerIntegrationTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void shouldPostLibraryEvent() {
+
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Diogo")
+                .bookName("Kafka using Spring Boot")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                        .libraryEventId(null)
+                                .book(book)
+                                .build();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("content-type", MediaType.APPLICATION_JSON.toString());
+        HttpEntity<LibraryEvent> request = new HttpEntity<>(libraryEvent);
+
+        ResponseEntity<LibraryEvent> responseEntity = restTemplate.exchange("/v1/libraryevent", HttpMethod.POST, request, LibraryEvent.class);
+
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+    }
+}
